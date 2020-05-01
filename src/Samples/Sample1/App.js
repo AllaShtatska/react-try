409b3db744cbbfba5e463574111550dd2b5d270a
Alla Shtatska <ashtatska@attune.gshapps.net>
2020-05-01 14:50:33 +0300

    Added some samples

---
+++ src/Samples/Sample1/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const element = (
+    <h1>Hello, world!</h1>
+);
+
+export default element;
+//ReactDOM.render(element, document.getElementById('root'));
---
+++ src/Samples/Sample2/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+  
+const user = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+};
+  
+function getGreeting(user){
+    if (user){
+        return <h1>Hello, {formatName(user)}!</h1>;
+    }
+
+    return <h1>Hello, Stranger.</h1>;
+}
+
+const element = getGreeting(user);
+export default element;
+//ReactDOM.render(element, document.getElementById('root'));
